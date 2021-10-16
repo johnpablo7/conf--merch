231bf6a2671ce743137bc9daf798c83269220930
John <john.davis.2015.03@gmail.com>
2021-10-16 11:16:29 -0500

    [add] react-helmet

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 import AppContext from '../context/AppContext';
 import '../styles/components/Checkout.css';
 
@@ -19,34 +20,39 @@ const Checkout = () => {
 	};
 
 	return (
-		<div className="Ckeckout">
-			<div className="Checkout-content">
-				{cart.length > 0 ? (
-					<h3>Lista de pedidos:</h3>
-				) : (
-					<h3>Sin pedidos...</h3>
-				)}
-				{cart.map((item) => (
-					<div className="Checkout-item">
-						<div className="Checkout-element">
-							<h4>{item.title}</h4>
-							<span>${item.price}</span>
+		<>
+			<Helmet>
+				<tile>Lista de pedidos - Platzi Conf Merch</tile>
+			</Helmet>
+			<div className="Ckeckout">
+				<div className="Checkout-content">
+					{cart.length > 0 ? (
+						<h3>Lista de pedidos:</h3>
+					) : (
+						<h3>Sin pedidos...</h3>
+					)}
+					{cart.map((item) => (
+						<div className="Checkout-item">
+							<div className="Checkout-element">
+								<h4>{item.title}</h4>
+								<span>${item.price}</span>
+							</div>
+							<button type="button" onClick={handleRemove(item)}>
+								<i className="fas fa-trash-alt" title="Eliminar" />
+							</button>
 						</div>
-						<button type="button" onClick={handleRemove(item)}>
-							<i className="fas fa-trash-alt" title="Eliminar" />
-						</button>
-					</div>
-				))}
-			</div>
-			{cart.length > 0 && (
-				<div className="Checkout-sidebar">
-					<h3>{`Precio Total: $ ${handleSumTotal()}`}</h3>
-					<Link to="/checkout/information">
-						<button type="button">Continuar Pedido</button>
-					</Link>
+					))}
 				</div>
-			)}
-		</div>
+				{cart.length > 0 && (
+					<div className="Checkout-sidebar">
+						<h3>{`Precio Total: $ ${handleSumTotal()}`}</h3>
+						<Link to="/checkout/information">
+							<button type="button">Continuar Pedido</button>
+						</Link>
+					</div>
+				)}
+			</div>
+		</>
 	);
 };
 
